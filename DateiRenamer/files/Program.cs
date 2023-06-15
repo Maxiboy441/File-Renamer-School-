26cd1dccf0e71560a90708101beb8ee085e10fee
Maximilian Huber (mahu) <maximilian.huber@odt.net>
2023-06-15 09:31:17 +0200

    add feature

--- DateiRenamer/files/Program.cs
+++ DateiRenamer/files/Program.cs
@@ -1,3 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
 
+public class FileRenamer
+{
+    public static void Main()
+    {
+    }
+
+
+    public static void RenameFile(string filePath, string newFileName)
+    {
+        if (!File.Exists(filePath))
+        {
+            Console.WriteLine("File does not exist.");
+            return;
+        }
+
+        string directory = Path.GetDirectoryName(filePath);
+        string fileExtension = Path.GetExtension(filePath);
+        string newFilePath = Path.Combine(directory, newFileName + fileExtension);
+
+        try
+        {
+            File.Move(filePath, newFilePath);
+            Console.WriteLine("File renamed successfully.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error occurred while renaming the file: {ex.Message}");
+        }
+    }
+}
