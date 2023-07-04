e97ebf01e75947641fceac1119691b146ae54b4b
veronika <veronika.wolff@its-stuttgart.de>
2023-07-04 10:44:35 +0200

    fix function

---
+++ DateiRenamer/RenamerTests/RenumberRenamerTest.cs
@@ -0,0 +1,59 @@
+﻿namespace RenamerTests
+{
+    [TestFixture]
+    public class RenumberRenamerTest
+    {
+        private string TestDirectoryPath;
+        private RenumberRenamer renumberRenamer;
+
+        [SetUp]
+        public void Setup()
+        {
+            TestDirectoryPath = Path.Combine(TestContext.CurrentContext.TestDirectory, "TestDirectory");
+            Directory.CreateDirectory(TestDirectoryPath);
+
+            string path1 = Path.Combine(TestDirectoryPath, "1-img.txt");
+            string path2 = Path.Combine(TestDirectoryPath, "3-img.txt");
+            string path3 = Path.Combine(TestDirectoryPath, "4img.txt");
+            string path4 = Path.Combine(TestDirectoryPath, "12-img.txt");
+            string path5 = Path.Combine(TestDirectoryPath, "5-img.txt");
+
+            File.WriteAllText(path1, "Text");
+            File.WriteAllText(path2, "Text");
+            File.WriteAllText(path3, "Text");
+            File.WriteAllText(path4, "Text");
+            File.WriteAllText(path5, "Text");
+
+            renumberRenamer = new RenumberRenamer();
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            Directory.Delete(TestDirectoryPath, true);
+        }
+
+
+        [Test]
+        public void RenumberRenamer_ThrowsException_WhenAbsoluteFilePathIsNull()
+        {
+            // Arrange
+            string absoluteFilePath = null;
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => renumberRenamer.execute(absoluteFilePath));
+        }
+
+        [Test]
+        public void RenumberRenamer_ThrowsException_WhenAbsoluteFilePathIsEmpty()
+        {
+            // Arrange
+            string absoluteFilePath = string.Empty;
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => renumberRenamer.execute(absoluteFilePath));
+
+
+        }
+    }
+}
--- DateiRenamer/files/RenumberRenamer.cs
+++ DateiRenamer/files/RenumberRenamer.cs
@@ -1,43 +1,80 @@
 ﻿using System.Text.RegularExpressions;
 
 public class RenumberRenamer
-{ 
+{
+    private string newFileName;
+    private string newFilePath;
+
     public void execute(string DirectoryPath)
     {
-        //Fehler abfangen
+        //catch exceptions
         if (string.IsNullOrEmpty(DirectoryPath)) { throw new ArgumentException("Absolute file path cannot be null or empty."); }
         if (!Directory.Exists(DirectoryPath)) { throw new Exception("Path doesn't exist."); }
 
 
-        int counter = 1;
-
-        //Umbenennen der Files
-        foreach (string originalFile in Directory.GetFiles(DirectoryPath))
+        //arrange
+        string[] files = Directory.GetFiles(DirectoryPath);
+        string[] filenames = getSortedFileNames(files);
+        
+        //rename
+        foreach (string originalFileName in filenames)
         {
-            string originalFileName = Path.GetFileName(originalFile);
-
-            if (Regex.IsMatch(originalFileName, "^[0-9]+-*"))
-            {
-                string newFileName = Regex.Replace(originalFileName, "^[0-9]+-*", $"{counter}-");
-                string newFilePath = Path.Combine(DirectoryPath, newFileName);
-
-                Console.WriteLine(newFileName);
-                
-                if (File.Exists(newFilePath))
-                {
-                    throw new InvalidOperationException($"A file with the name '{newFileName}' already exists.");
-                }
-
-                File.Move(originalFile, newFilePath);
-                Console.WriteLine($"The file '{originalFileName}' renamed to '{newFileName}'.");
-                
-                counter++;
-            }
-
-            
+            rename(DirectoryPath, originalFileName, filenames.Length);
         }
 
         Console.WriteLine();
         Console.WriteLine("Executed!");
+    }
+
+    private void rename(string DirectoryPath,string originalFileName, int fileNumber)
+    {
+        //arrange
+        for (int i = 1; i <= fileNumber; i++)
+        {
+            newFileName = Regex.Replace(originalFileName, "^[0-9]+-*", $"{i}-");
+            newFilePath = Path.Combine(DirectoryPath, newFileName);
+            if (newFileName == originalFileName) { break; }
+            if (!File.Exists(newFilePath)) { break; }
+        }
+
+        //move
+        if (newFileName != originalFileName) {
+            string originalFilePath = Path.Combine(DirectoryPath, originalFileName);
+
+            moveFile(originalFilePath, newFilePath);
+            Console.WriteLine($"The file '{originalFileName}' was renamed to '{newFileName}'.");
+        }
+    }
+
+    private void moveFile(string originalFile, string newFilePath)
+    {
+        try
+        {
+            File.Move(originalFile, newFilePath);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error renaming file: {originalFile}. {ex.Message}");
+        }
+    }
+
+    private string[] getSortedFileNames(string[] files)
+    {
+        string[] fileNamesTemp = new string[files.Length];
+        int counter2 = 0;
+        foreach (string f in files)
+        {
+            if (Regex.IsMatch(Path.GetFileName(f), "^[0-9]+-*"))
+            {
+                fileNamesTemp[counter2] = Path.GetFileName(f);
+                counter2++;
+            }
+        }
+        string[] fileNames = fileNamesTemp.Where(c => c != null).ToArray();
+
+
+        Array.Sort(fileNames, (a, b) => int.Parse(Regex.Replace(a, "[^0-9]", "")) - int.Parse(Regex.Replace(b, "[^0-9]", "")));
+
+        return fileNames;
     }
 }
